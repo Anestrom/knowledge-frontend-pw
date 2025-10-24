8814d5525dec501ffd0afdbf53dbe7bd4e0bd7af
Marjory <toazzamarjory12@gmail.com>
2025-10-24 08:32:34 -0300

    Atualização da pagina Home

--- src/componentes/telas/Home.jsx
+++ src/componentes/telas/Home.jsx
@@ -1,6 +1,9 @@
 const Home = () => (
     <div>
         <h1>Home - Knowledge</h1>
+        <h2>Conceito</h2>
+        <p>O Knowledge funciona como um hub de mentoria entre pares. A plataforma conecta, em tempo real, alunos que precisam de apoio 
+            urgente em uma matéria específica (Aprendizes) com colegas de cursos mais avançados ou com domínio no assunto (Mentores).</p>
     </div>
 );
 export default Home;
